298be25dcb2e0b7d36940a4a0b78e3aaaabba9ee
Piotrek <ptrlechowicz@gmail.com>
2022-09-05 18:05:07 +0200

    created desktop navigation and scripts for mobile one

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 const navItems = document.querySelector('.nav__items');
-const navBtn = document.querySelector('.nav__btn');
+const navBtn = document.querySelector('.hamburger');
+const allNavItems = document.querySelectorAll('.nav__link');
 
 navBtn.addEventListener('click', () => {
     navItems.classList.toggle('nav--active');
+    navBtn.classList.toggle('is-active');
+    document.body.classList.toggle('sticky-body');
+})
+
+allNavItems.forEach(el => {
+    el.addEventListener('click', () => {
+        navItems.classList.remove('nav--active');
+        navBtn.classList.remove('is-active');
+    })
 })
